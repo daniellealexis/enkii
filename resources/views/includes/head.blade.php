758e9005c4c47203acecdc8f038a2f604a05f5e4
Dani Lewandowski <dani@splashthat.com>
2017-05-15 23:51:49 -0400

    Added app name to laravel app config. Using it in default layout.

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,7 +2,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<title>{{ $title or 'Enkii' }}</title>
+<title>{{ $title or config('app.name', 'Enkii') }}</title>
 
 <link href="{{ secure_asset('css/app-main.css') }}" rel="stylesheet" type="text/css">
 @stack('styles')
