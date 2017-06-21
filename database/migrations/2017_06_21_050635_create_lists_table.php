1fce98876ab74f9d9b6ab02aba2ba80cedf2f0e3
Dani Lewandowski <dani@splashthat.com>
2017-06-21 01:43:01 -0400

    Created migrations to add lists and list_items tables. Created models for them.

---
+++ database/migrations/2017_06_21_050635_create_lists_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('lists')) {
+            Schema::create('lists', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->unsignedInteger('user_id');
+                $table->timestampsTz();
+                $table->text('title')->default('');
+                $table->mediumText('description')->default('');
+                $table->text('image_url')->nullable();
+                $table->enum('type', ['ol', 'ul'])->default('ul');
+
+                $table->foreign('user_id')->references('id')->on('users');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasTable('lists')) {
+            Schema::drop('lists');
+        }
+    }
+}
